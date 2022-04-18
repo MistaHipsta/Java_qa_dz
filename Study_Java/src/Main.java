dbb20558f478a922982d17c90dc23d386def9a3c
MistaHipsta <50655466+MistaHipsta@users.noreply.github.com>
2022-04-19 00:36:01 +0300

    Add files via upload

---
+++ Study_Java/src/Main.java
@@ -0,0 +1,53 @@
+import com.sun.security.jgss.GSSUtil;
+
+import java.util.Scanner;
+
+public class Main {
+
+   public static void main(String[] args) {
+
+      System.out.println("Введите ваше число: ");
+
+      Scanner scanner = new Scanner(System.in);
+      int number = scanner.nextInt();
+
+      if (number < 10 && number>-10) {
+
+         System.out.println("Ваше число однозначное");
+
+         if (number > 0) {
+
+            System.out.println("Ваше число положительное");
+         }
+         else {
+            System.out.println("Ваше число отрицательное");
+         }
+      }
+
+       else if (number < 100&& number>-100) {
+         System.out.println("Ваше число двухзначное");
+         if (number > 0) {
+
+            System.out.println("Ваше число положительное");
+         }
+         else {
+            System.out.println("Ваше число отрицательное");
+         }
+
+      }
+       else if (number < 1000&& number>-1000) {
+         System.out.println("Ваше число трехзначное");
+         if (number > 0) {
+
+            System.out.println("Ваше число положительное");
+         }
+         else {
+            System.out.println("Ваше число отрицательное");
+         }
+      }
+      else {
+         System.out.println("Ваше число четырехзначное или больше");
+      }
+   }
+}
+
