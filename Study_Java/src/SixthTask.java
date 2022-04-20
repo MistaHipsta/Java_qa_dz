e59f7acb7876b4dea13db91c500dde4c16d97c2e
EvgeniiSuv <evgenii.suvorov@winfinity.live>
2022-04-20 17:55:18 +0300

    qwe

--- Study_Java/src/SixthTask.java
+++ Study_Java/src/SixthTask.java
@@ -7,19 +7,10 @@ public class SixthTask {
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
         int tail = number%10;
-switch (tail){
-    case 1:
-        System.out.println(number + " Програмист");
-        break;
-    case 2:
-        System.out.println(number + " Програмиста");
-        break;
-    case 3:
-        System.out.println(number + " Програмиста");
-        break;
-    case 4:
-        System.out.println(number + " Програмиста");
-        break;
-}
+
+        if (){
+            System.out.println(number + " Програмист");
+        }
+
     }
 }
