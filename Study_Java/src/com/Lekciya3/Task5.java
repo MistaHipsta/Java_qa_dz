e895109ee0df7ccfc8a3c48335a6114248edae19
EvgeniiSuv <evgenii.suvorov@winfinity.live>
2022-04-27 15:15:48 +0300

    fix

--- Study_Java/src/com/Lekciya3/Task5.java
+++ Study_Java/src/com/Lekciya3/Task5.java
@@ -9,27 +9,30 @@ public class Task5 {
         Random random = new Random();
 
         int summa1 = 0;
-
+        int kolichestvo = 0;
         for (int i = 0; i < masiv1.length; i++) {
             masiv1[i] = random.nextInt(15);
             System.out.print(masiv1[i] + " ");
             summa1 = summa1 + masiv1[i];
+            kolichestvo++;
 
         }
         System.out.println();
-        int srednee = summa1 / 5;
-        System.out.println("Среднее арифметическое первого массива " + (double)srednee);
+        double srednee = summa1 / kolichestvo;
+        System.out.println("Среднее арифметическое первого массива " + srednee);
         System.out.println();
 
         int summa2 = 0;
+        int kolichestvo2 = 0;
         for (int i = 0; i < masiv2.length; i++) {
             masiv2[i] = random.nextInt(15);
             System.out.print(masiv2[i] + " ");
             summa2 = summa2 + masiv2[i];
+            kolichestvo2++;
         }
         System.out.println();
-        int srednee2 = summa2 / 5;
-        System.out.println("Среднее арифметическое второго массива " + (double)srednee2);
+        double srednee2 = summa2 / kolichestvo2;
+        System.out.println("Среднее арифметическое второго массива " + srednee2);
         System.out.println();
 
         if (srednee>srednee2){
